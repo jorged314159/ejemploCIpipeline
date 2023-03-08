cddf10140fa7335e5384537982d82d1a300bdcba
jorged314159 <jorged314159@gmail.com>
2023-03-08 12:52:10 -0600

    Agregando pruebas unitarias y configuracion de jenkins

--- mensajes/tests.py
+++ mensajes/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestHola(TestCase):
+    
+    def test_humo(self):
+        self.assertEqual(2, 1+1)
+
+    def test_request_url_hola(self):
+        respuesta = self.client.get('/hola/')
+        self.assertEqual(respuesta.status_code, 200)
